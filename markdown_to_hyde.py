07cb8fd9ed4a221c831c3449a822b870ff6ac47c
Rowyn McDonald <rowynm@gmail.com>
2011-07-21 15:19:40 -0700

    Draft version of script to create Hyde templates for wiki markdown files.

---
+++ markdown_to_hyde.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import glob
+import optparse
+import os
+import re
+import string
+
+PROG_ROOT = os.path.dirname(os.path.realpath( __file__ ))
+
+HYDE_RE = re.compile(r'({%hyde.+?%})(.+)', re.DOTALL)
+
+TEMPLATE = string.Template("""{% extends "_post.html" %}
+
+$hyde_tag
+
+{% block article %}
+
+{% include 'patterns/$filename' %}
+
+{% endblock %}""")
+
+
+def HtmlFilename(basename):
+    return basename.split('.')[0] + '.html'
+
+
+def ProcessMarkdown(filename):
+    """Takes the name of a markdown file and returns a tuple consisting of two
+    strings, the initial Hyde tag and the remaining contents of the file."""
+    f = open(filename, 'r')
+    contents = f.read()
+    result = HYDE_RE.match(contents)
+    if result and result.groups():
+        return (result.group(1), result.group(2).strip())
+    else:
+        print "No hyde tag found in file: %s" % filename
+        return (None, None)
+
+
+def ProcessFiles(source_dir, dest_dir):
+    for filename in glob.glob(os.path.join(source_dir, '*.md')):
+        basename = os.path.basename(filename)
+        hyde_tag, contents = ProcessMarkdown(filename)
+        if hyde_tag and contents:
+            hyde_file = open(os.path.join(dest_dir, HtmlFilename(basename)), 'w')
+            hyde_file.write(
+                TEMPLATE.safe_substitute(hyde_tag=hyde_tag, filename=basename))
+            hyde_file.close()
+
+            md_file = open(os.path.join(dest_dir, basename), 'w')
+            md_file.write(contents)
+            md_file.close()
+            print "Successfully processed file: %s" % filename
+
+            
+def main():
+    parser = optparse.OptionParser(usage="%prog [-f] [-q]", version="%prog 0.5.3")
+    parser.add_option("-s", "--source",
+                        dest = "source_dir",
+                        help = "Path of the source folder containing markdown files.")
+    parser.add_option("-d", "--destination",
+                        dest = "dest_dir",
+                        help = "Path of the destination folder in which to create html templates.")
+
+    (options, args) = parser.parse_args()
+
+    if len(args):
+        parser.error("Unexpected arguments encountered.")
+
+    if not options.source_dir:
+        options.source_dir = os.getcwdu()
+
+    if options.dest_dir:
+        options.dest_dir = os.path.abspath(options.dest_dir)
+
+    ProcessFiles(options.source_dir, options.dest_dir)
+    
+
+if __name__ == "__main__":
+    main()
